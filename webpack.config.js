d9717bbc8031040bf450ed4cece448124565a146
Alexander Sokolov <thorpe.nulled@gmail.com>
2022-06-26 20:11:15 +0300

    feat: add info to readme, tune gitignore, install node-loader, fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const ESLintPlugin = require("eslint-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 const webpack = require('webpack');
 const dotenv = require('dotenv')
 dotenv.config();
@@ -16,26 +16,26 @@ module.exports = {
         test: /\.ts$/,
         use: "ts-loader",
         exclude: /node_modules/
+      },
+      {
+        test: /\.node$/,
+        use: "node-loader",
       }
     ]
   },
   plugins: [
-    new ESLintPlugin({ extensions: ["ts"] }),
+    new ESLintPlugin({extensions: ["ts"]}),
     new webpack.DefinePlugin({
       'process.env': JSON.stringify(process.env)
     }),
-    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false })
+    new CleanWebpackPlugin({cleanStaleWebpackAssets: false})
   ],
   resolve: {
-    extensions: [".ts"],
-    fallback: {
-      "dotenv": false,
-      "http": false,
-      "path": false,
-      "fs": false,
-      "os": false,
-      "process": false
-    }
+    extensions: [".js", ".ts"],
+  },
+  externals: {
+    bufferutil: "bufferutil",
+    "utf-8-validate": "utf-8-validate",
   },
   output: {
     filename: "index.js",
